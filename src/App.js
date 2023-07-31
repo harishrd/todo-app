849b1f50a689bb378ca2595d360239923376b3c3
harishrd <harishrd321@gmail.com>
2023-07-31 15:50:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from 'react';
+
+
+function App() {
+  return (
+    <div className="container">
+      <h2>MERN-Stack Todo App</h2>
+    </div>
+  );
+}
+
+export default App;
